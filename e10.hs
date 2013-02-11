f22f6ad4bdc44389c0b306a89d1f5d4ec4ce3c1d
cowlike <cowlike@gmail.com>
2013-02-10 21:53:51 -0600

    removed e7.hs and e10.hs since they are all in euler.hs

--- e10.hs
+++
@@ -1,7 +0,0 @@
-import Utils
-
--- sum all primes with a value < x
--- use 2000000 for problem #10
-
-primeSum :: Int -> Int
-primeSum x = sum $ takeWhile ((>) x) $ filter prime [2..]
--- e7.hs
+++
@@ -1,7 +0,0 @@
-import Utils
-
--- n = 10001 for problem #10
-
-getPrime :: Int -> Int
-
-getPrime n = head . drop (n - 1) . filter prime $ [2..]
