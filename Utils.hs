30f3e94d71d950d04c2c4ca85d95a5e0a8030c4a
Krupka <u273693@WWHQ8888.global.ual.com>
2013-02-14 13:44:35 -0600

    fixed bug in get, turned all tabs to spaces

--- Utils.hs
+++ Utils.hs
@@ -8,26 +8,26 @@ isPrime
 isPrime :: Integral a => a -> Bool
 isPrime x
       | x < 2 = False
-	  | x == 2 || x == 3 = True
-	  | x `mod` 2 == 0 || x `mod` 3 == 0 = False
+      | x == 2 || x == 3 = True
+      | x `mod` 2 == 0 || x `mod` 3 == 0 = False
 
 isPrime x = foldl (\acc n -> (not $ x `mod` n == 0) && acc) True [5,7..o]
-	      where   
-		     f = floor $ sqrt $ fromIntegral x
-		     o = if even f then f + 1 else f
-		
+          where   
+             f = floor $ sqrt $ fromIntegral x
+             o = if even f then f + 1 else f
+        
 -- **************************************************************
 isPrime' :: Integral a => a -> Bool
 isPrime' x
       | x < 2 = False
-	  | x == 2 || x == 3 = True
-	  | x `mod` 2 == 0 || x `mod` 3 == 0 = False
+      | x == 2 || x == 3 = True
+      | x `mod` 2 == 0 || x `mod` 3 == 0 = False
 
 isPrime' x = foldr (\n acc -> (not $ x `mod` n == 0) && acc) True [5,7..o]
-	      where   
-		     f = floor $ sqrt $ fromIntegral x
-		     o = if even f then f + 1 else f
-			 
+          where   
+             f = floor $ sqrt $ fromIntegral x
+             o = if even f then f + 1 else f
+             
 -- **************************************************************
 fib :: Int -> Int
 fib 0 = 1
@@ -37,4 +37,4 @@ fib n = fib (n-1) + fib (n-2)
 partition :: [a] -> Int -> Int -> [[a]]
 partition str partBy step 
           | length str < partBy = []
-		  | otherwise = take partBy str : partition (drop step str) partBy step
+          | otherwise = take partBy str : partition (drop step str) partBy step
